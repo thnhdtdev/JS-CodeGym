785cb53fdc06aa4b7416e2ac2880f2a90415720b
DatVo <vodat0301@gmail.com>
2023-10-29 13:10:42 +0700

    create lab2

---
+++ Lab_JS_CodeGym/W02/Lab02/script.js
@@ -0,0 +1,8 @@
+let btnAction1 = document.getElementById('btn');
+
+btnAction1.addEventListener('click', () => {
+
+    let yourName = prompt("Hãy nhập tên của bạn");
+
+    alert("Hello" + yourName)
+});
