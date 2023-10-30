c7ed3bb88b5aa34a822b0e66beada80c27c45c07
DatVo <vodat0301@gmail.com>
2023-10-30 21:46:41 +0700

    create w14

---
+++ Buoi14 - 30-10-2023/script.js
@@ -0,0 +1,40 @@
+//Bai 1
+// let i = 10;
+
+// while (i >= -10) {
+//     console.log(i);
+//     i--;
+// }
+
+
+
+
+
+//Bai 2
+// let i = 0;
+
+// while (i <= 100) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+
+//nhap ten tren 6 ky tu thi ok
+
+//Bai 3
+// let fullName = prompt("Nhap ho va ten: ");
+// while (fullName.length < 6) {
+//     alert("error");
+//     fullName = prompt("Nhap ho va ten: ");
+// }
+// console.log(fullName);
+
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 5 === 0) {
+        console.log(i);
+    }
+}
