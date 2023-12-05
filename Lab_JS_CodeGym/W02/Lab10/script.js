5b449e58950f999f9fc1d397c404c0c4a26d2905
DatVo <vodat0301@gmail.com>
2023-12-05 22:58:55 +0700

    create btvn

---
+++ Lab_JS_CodeGym/W02/Lab10/script.js
@@ -0,0 +1,18 @@
+// let num = prompt("Enter a number: ");
+// let total = 0;
+
+
+// while (num != -1) {
+//     num = prompt("Enter a number: ");
+//     alert(num);
+//     total += num;
+// } 
+
+// bai 2
+let i = 1;
+
+while (i < 100) {
+    // phân thân vòng lặp
+    document.write("<hr width = " + i + "%>");
+    i++;
+}
