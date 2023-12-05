cf21dc81f5797e56f3a9e1f87223df40c78942ac
DatVo <vodat0301@gmail.com>
2023-12-05 22:34:44 +0700

    create learn codegym

---
+++ Lab_JS_CodeGym/W02/Lab05/script.js
@@ -0,0 +1,21 @@
+/*
+let a = prompt("a: ");
+let b = prompt("b: ");
+let message;
+
+result = (a + b < 4) ? 'Below' : 'Over';
+
+*/
+
+//Bai 2
+
+let message = (login == 'Employee') ?
+    'Hello' :
+    (login == 'Director') ?
+        'Greetings' :
+        (login == '') ?
+            'No login' :
+            '';
+
+alert(message)
+
